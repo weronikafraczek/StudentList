5b2d6c4a582fced92287d91fbb0d99688c687fb6
weronikafraczek <weronikaif@gmail.com>
2021-10-28 17:51:16 +0200

    Initial commit

---
+++ src/main/java/weronikafraczek/StudentList/StudentListApplication.java
@@ -0,0 +1,13 @@
+package weronikafraczek.StudentList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/weronikafraczek/StudentList/StudentListApplicationTests.java
@@ -0,0 +1,13 @@
+package weronikafraczek.StudentList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
